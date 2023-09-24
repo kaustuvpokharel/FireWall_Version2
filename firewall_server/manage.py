b748621402f5a99e3476183954a664f2bc6fd935
Bhaskar Neupane <39903236+vaskrneup@users.noreply.github.com>
2023-09-24 12:11:40 -0400

    FINAL

--- firewall_server/manage.py
+++ firewall_server/manage.py
@@ -3,6 +3,3 @@ from server import Brain
 if __name__ == "__main__":
     brain = Brain("192.168.207.148", 1234, display_logs=True)
     brain.accept_requests()
-
-# kanav.kahol@bell.ca
-# 647 292 3991
